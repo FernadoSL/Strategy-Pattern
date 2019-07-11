01e481191b8acdade39b0fb7305d3559a092374a
FernandoSL <nando.leandro@hotmail.com>
2019-07-11 10:32:44 -0300

    Initial Commit
    The first classes to implement the pattern.

---
+++ Strategy Pattern/ApiClient.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Strategy_Pattern
+{
+    public class ApiClient
+    {
+        private static readonly HttpClient client;
+        private static readonly XmlSerializer serializer;
+
+        static ApiClient()
+        {
+            client = new HttpClient();
+            var xRoot = new XmlRootAttribute
+            {
+                ElementName = "Order Summary"
+            };
+            serializer = new XmlSerializer(typeof(OrderSummary), xRoot);
+        }
+
+        public async Task<HttpStatusCode> SendOrderSummary(string uri, OrderSummary orderSummary)
+        {
+            using StringWriter textWriter = new StringWriter();
+            serializer.Serialize(textWriter, orderSummary);
+            string orderSummaryString = textWriter.ToString();
+
+            var httpContent = new StringContent(orderSummaryString, Encoding.UTF8, "application/xml");
+            var response = await client.PostAsync(uri, httpContent);
+
+            return response.StatusCode;
+        }
+    }
+}
---
+++ Strategy Pattern/OrderSummary.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Strategy_Pattern
+{
+    public class OrderSummary
+    {
+        public int UserId { get; set; }
+
+        public int ItemId { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+    }
+}
